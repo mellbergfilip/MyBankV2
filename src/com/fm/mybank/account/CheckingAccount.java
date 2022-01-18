6e34534a46ce3951563311127415f84bf7b2202e
Filip Mellberg <filipmellberg@outlook.com>
2022-01-18 19:53:32 +0100

    Added interface for bank accounts

--- src/com/fm/mybank/account/CheckingAccount.java
+++ src/com/fm/mybank/account/CheckingAccount.java
@@ -16,6 +16,30 @@ public class CheckingAccount extends BankAccount {
 		setAccountNr(accountNr);
 	}
 
+	/*
+	 * Method that takes double as parameter, controls if double is >= 0, adding
+	 * double to balance
+	 */
+	public void deposit(double sum) {
+		if (sum >= 0) {
+			balance = balance + sum;
+		} else {
+			throw new IllegalArgumentException("You can't deposit a negative sum.");
+		}
+	}
+
+	/*
+	 * Method that takes a double as parameter, controls if double is >= 0, taking
+	 * double from balance
+	 */
+	public void withdraw(double sum) {
+		if (sum >= 0) {
+			balance = balance - sum;
+		} else {
+			throw new IllegalArgumentException("You can't withdraw a negative sum.");
+		}
+	}
+
 	@Override
 	public String toString() {
 		return "\nType: Checking account" + "\nAccountnr: " + accountNr + "\nBalance: " + balance + " $\n";
